816b79b5ecd9adb7da82887a607d3a3889b7e225
locphp <hellophp@qq.com>
2017-04-21 23:09:16 +0800

    -

---
+++ src/Controller.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Xcs;
+
+class Controller {
+
+    //用户信息
+    protected $login_user = null;
+    //当前控制器
+    protected $_ctl;
+    //当前动作
+    protected $_act;
+    //时间戳
+    protected $timestamp;
+
+    /*
+     * 初始执行
+     */
+    public function __construct($controllerName, $actionName) {
+        $this->_ctl = $controllerName;
+        $this->_act = $actionName;
+        $this->init_var();
+        $this->init_cache();
+        //$this->init_timezone();
+    }
+
+    public function __destruct() {
+
+    }
+
+    public function __call($name, $arguments) {
+        //动作不存在
+        if (App::isAjax(true)) {
+            $retarr = array(
+                'errcode' => 1,
+                'errmsg' => 'Action ' . $name . '不存在!',
+                'data' => ''
+            );
+            return rep_send($retarr, 'json');
+        }
+        $args = 'Action:' . $name . "不存在";
+        include template('404');
+    }
+
+    /*
+     * 初始变量
+     */
+
+    private function init_var() {
+        $this->timestamp = getgpc('s.REQUEST_TIME') ?: time();
+        if (filter_input(INPUT_GET, 'page')) {
+            $_GET['page'] = max(1, filter_input(INPUT_GET, 'page'));
+        }
+    }
+
+    /*
+     * 初始缓存
+     */
+
+    private function init_cache() {
+        $caches = getini('cache/default');
+        loadcache($caches);
+    }
+
+    /*
+     * 时区
+     */
+    private function init_timezone() {
+        //php > 5.1
+        $timeoffset = getini('settings/timezone');
+        $timeoffset && date_default_timezone_set('Etc/GMT' . ($timeoffset > 0 ? '-' : '+') . abs($timeoffset));
+    }
+
+    final function checklogin() {
+        if ($this->login_user) {
+            return $this->login_user;
+        }
+        $this->login_user = User::getUser();
+        return $this->login_user;
+    }
+
+    final function checkacl($controllerName, $actionName, $auth = AUTH) {
+        return Rbac::check($controllerName, $actionName, $auth);
+    }
+
+}
