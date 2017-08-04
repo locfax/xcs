554aff30e230963f9276855127104eddd2a11b13
locphp <hellophp@qq.com>
2017-08-04 20:37:48 +0800

    -

--- src/Context.php
+++ src/Context.php
@@ -4,27 +4,25 @@ namespace Xcs;
 
 class Context {
 
-    private static $_dsns = array();
-    private static $_configs = array();
-
     /**
      * @param $dsnid
      * @return mixed
      */
     public static function dsn($dsnid) {
-        if (!isset(self::$_dsns[APPKEY])) {
+        static $_dsns = array();
+        if (!isset($_dsns[APPKEY])) {
             $dsns = App::mergeVars('dsn');
             foreach ($dsns as $key => $dsn) {
                 $dsns[$key]['dsnkey'] = md5(APPKEY . '_' . $key . '_' . $dsn['driver'] . '_' . $dsn['dsn']); //连接池key
             }
-            self::$_dsns[APPKEY] = $dsns;
-            if (!isset(self::$_dsns[APPKEY][$dsnid])) {
-                self::$_dsns[APPKEY][$dsnid] = array();
+            $_dsns[APPKEY] = $dsns;
+            if (!isset($_dsns[APPKEY][$dsnid])) {
+                $_dsns[APPKEY][$dsnid] = array();
             }
             $dsns = null;
         }
         //如果没配置$dsnid 会报错
-        return self::$_dsns[APPKEY][$dsnid];
+        return $_dsns[APPKEY][$dsnid];
     }
 
     /**
@@ -33,8 +31,9 @@ class Context {
      * @return bool|mixed
      */
     public static function config($name, $type = 'inc') {
+        static $_configs = array();
         $key = APPKEY . '.' . $name . '.' . $type;
-        if (isset(self::$_configs[$key])) {
+        if (isset($_configs[$key])) {
             return self::$_configs[$key];
         }
         $file = PSROOT . '/config/' . strtolower($name) . '.' . $type . '.php';
@@ -46,12 +45,4 @@ class Context {
         return self::$_configs[$key];
     }
 
-    /**
-     * @param string $data
-     * @param int $code
-     */
-    public static function log($data, $code = 0) {
-        $logfile = DATAPATH . 'log/run.log';
-        Helper\Log::writeLog($logfile, $data);
-    }
 }
