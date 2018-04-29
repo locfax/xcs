255f8b4901baa8e3c1a1efb1e471020751006d0b
locphp <hellophp@qq.com>
2018-04-29 17:20:03 +0800

    -

--- src/Database/Pdo.php
+++ src/Database/Pdo.php
@@ -24,7 +24,7 @@ class Pdo {
             \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
             \PDO::ATTR_EMULATE_PREPARES => false,
             \PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
-            //\PDO::MYSQL_ATTR_USE_BUFFERED_QUERY => true,
+            \PDO::MYSQL_ATTR_USE_BUFFERED_QUERY => true,
             \PDO::ATTR_PERSISTENT => false
         );
         try {
